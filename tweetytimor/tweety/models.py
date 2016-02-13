ef69040e50cbf09a28f405c8d2f06aa4a1beee85
marobo <dejesushonorio03@gmail.com>
2016-02-13 09:27:51 +0900

    Tweety Timor

--- tweetytimor/tweety/models.py
+++ tweetytimor/tweety/models.py
@@ -1,5 +1,8 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class Tweety(models.Model):
+    description = models.TextField(max_length=1000)
+    
--- tweetytimor/tweety/views.py
+++ tweetytimor/tweety/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic.base import TemplateView
+from tweety.models import Tweety
+from django.core.urlresolvers import reverse_lazy
 # Create your views here.
+
+
+class Index(TemplateView):
+    model = Tweety
+    fields = ['description']
+    template_name = 'tweety/index.html'
+    success_url = reverse_lazy('index')
