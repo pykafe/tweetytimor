fcb4b7a1df77b291271989bb3b2ad603ddbdca8c
Marioap <mariorabbap@gmail.com>
2016-02-18 14:09:55 +0900

    adding models, views and url

--- tweetytimor/tweety/models.py
+++ tweetytimor/tweety/models.py
@@ -2,4 +2,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class TweetyTimor(models.Model):
+    comment = models.TextField(max_length=400)
--- tweetytimor/tweety/views.py
+++ tweetytimor/tweety/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from tweety.models import TweetyTimor
+from django.core.urlresolvers import reverse_lazy
 
-# Create your views here.
+
+class AddTweet(CreateView):
+    model = TweetyTimor
+    fields = ['comment']
+    template_name = "tweety/tweety_index.html"
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(AddTweet, self).get_context_data(*args, **kwargs)
+        context['tweets'] = TweetyTimor.objects.all()
+        return context
