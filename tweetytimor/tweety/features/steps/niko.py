9c47b0e4ffe02bfea986f86461ba163df5783486
Joanico <joanicobarros@gmail.com>
2016-02-19 13:17:52 +0900

    modified scenario in niko.py

--- tweetytimor/tweety/features/steps/niko.py
+++ tweetytimor/tweety/features/steps/niko.py
@@ -1,10 +1,7 @@
-@then(u'I see the text')  # noqa
-def impl(context):
-    for row in context.table:
-        assert context.browser.is_text_present(row['text'])
+from behave import then
 
 
 @then(u'I see the text')  # noqa
 def impl(context):
-    for row in context.table:
-        assert context.browser.find_name('name').last
+    assert context.browser.is_text_present('text')
+    return True
