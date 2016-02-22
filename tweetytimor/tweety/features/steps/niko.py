56e77065f95082eb52bb1972771e2dfee4193d14
PeteCoward <peter.coward@gmail.com>
2016-02-22 14:58:44 +0900

    fixed tests and added plural text

--- tweetytimor/tweety/features/steps/niko.py
+++ tweetytimor/tweety/features/steps/niko.py
@@ -3,5 +3,5 @@ from behave import then
 
 @then(u'I see the text')  # noqa
 def impl(context):
-    assert context.browser.is_text_present('text')
-    return True
+    for row in context.table:
+        assert context.browser.is_text_present(row['text'])
