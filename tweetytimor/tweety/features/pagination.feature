c2f44f8483210168bd0af8d8658cc7cf2903c787
PeteCoward <peter.coward@gmail.com>
2016-02-22 17:00:03 +0900

    adding tests for pagination

--- tweetytimor/tweety/features/pagination.feature
+++ tweetytimor/tweety/features/pagination.feature
@@ -1,26 +1,15 @@
 Feature: Feature ba pajinasaun
     Scenario: Hau bele hare deit komentariu lima husi komentariu sira
         When I visit the website
-        When I type some text into the text entry box
-            | text |
-            | any text |
-        When I click the add button
-        Then I see the text
-            | text |
-            | there is 1 tweet |
-        When I type some text into the text entry box
-            | text |
-            | type any more text to five value |
-        When I click the add button
-        Then I see the five comment of tweety
-            | text |
-            | there is 1 tweet   |
-            | there are 2 tweets |
-            | there are 3 tweets |
-            | there are 4 tweets |
-            | there are 5 tweets |
-        When I click the add button
-        Then I see the button show_more 
-
+        When I add some tweets
+            | tweet |
+            | this is the first tweet |
+            | this is the second tweet |
+            | this is the third tweet |
+            | this is the fourth tweet |
+            | this is the fifth tweet |
+            | this is the sixth tweet |
+            | this is the seventh tweet |
+        Then I see only five tweets
         When I click the show_more button
-        Then I can see all of the tweety comments 
+        Then I can see more than five tweets 
--- tweetytimor/tweety/features/steps/pagination.py
+++ tweetytimor/tweety/features/steps/pagination.py
@@ -1,26 +1,33 @@
 from behave import when, then
 
 
-@then(u'I see the five comment of tweety')
-def impl(context):
-    for row in context.table:
-        assert context.browser.find_by_text(row['text'])
-
-
-@then(u'I see the button show_more')
-def impl(context):
-    show_more_button = context.browser.find_by_css('a[href^="/tweety/pagination/"]')
-    assert show_more_button
-
-
 @when(u'I click the show_more button')
 def impl(context):
-    show_more_button = context.browser.find_by_css('a[href^="/tweety/pagination/"]').click()
+    context.browser.find_by_css('a[href^="/tweety/pagination/"]').click()
 
 
-@then(u'I can see all of the tweety comments')
+def get_text_entry_box(context):
+    return context.browser.find_by_tag('textarea')
+
+
+def get_add_button(context):
+    return context.browser.find_by_css('input[type=submit]')
+
+
+@when(u'I add some tweets')
 def impl(context):
-    tweetlist = context.browser.find_by_tag('ul').first
-    assert tweetlist.visible
     for row in context.table:
-        assert tweetlist.find_by_text(row['text']).visible
+        get_text_entry_box(context).fill(row['tweet'])
+        get_add_button(context).click()
+
+
+@then(u'I can see more than five tweets')
+def impl(context):
+    tweets = context.browser.find_by_css('li.tweet')
+    assert len(tweets) > 5
+
+
+@then(u'I see only five tweets')
+def impl(context):
+    tweets = context.browser.find_by_css('li.tweet')
+    assert len(tweets) == 5
