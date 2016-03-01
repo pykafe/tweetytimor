a60cdd23994f042d13620f59d74726cc8436c606
Nando <nando22alia@gmail.com>
2016-03-01 18:58:53 +0900

    first commit

---
+++ tweetytimor/tweety/features/steps/tweety.py
@@ -0,0 +1,27 @@
+@then(u'the tweety text is visible')
+def impl(context):
+    assert False
+
+@when(u'I click the add button')
+def impl(context):
+    assert False
+
+@when(u'I type some text into the text entry box')
+def impl(context):
+    assert False
+
+@then(u'a text entry box is visible')
+def impl(context):
+    assert False
+
+@when(u'I visit the website')
+def impl(context):
+    assert False
+
+@then(u'some text is visible')
+def impl(context):
+    assert False
+
+@then(u'an Add button is visible')
+def impl(context):
+    assert False
---
+++ tweetytimor/tweety/models.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class TweetyTimor(models.Model):
+    comment = models.TextField(max_length=200)
