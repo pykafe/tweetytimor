ecd4602d30b7be53f26d2e4dbbc74c1af281d8c8
Joanico <joanicobarros@gmail.com>
2016-03-08 09:06:31 +0900

    added count for like tweety

--- tweetytimor/tweety/urls.py
+++ tweetytimor/tweety/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import url
-from django.contrib.auth.decorators import login_required
 from .views import TweetyCreate, LikeTweet, TweetComment, CreateMember
 
 
 urlpatterns = [
     url(r'^$', TweetyCreate.as_view(), name='tweety'),
-    url(r'^create/$', login_required(CreateMember.as_view()), name='create'),
+    url(r'^create/$', CreateMember.as_view(), name='create'),
     url(r'^like', LikeTweet.as_view(), name='like'),
     url(r'^comment', TweetComment.as_view(), name='comment'),
 ]
--- tweetytimor/tweety/views.py
+++ tweetytimor/tweety/views.py
@@ -1,6 +1,8 @@
 from django.views.generic.edit import CreateView, ModelFormMixin
 from tweety.models import TweetyTimor, Like, TweetComment, Member
 from django.core.urlresolvers import reverse_lazy
+from django.db.models import Count
+from django.http import HttpResponse
 
 
 class TweetyCreate(CreateView):
@@ -17,7 +19,7 @@ class TweetyCreate(CreateView):
 
     def get_context_data(self, **kwargs):
         context = super(TweetyCreate, self).get_context_data(**kwargs)
-        context['tweets'] = TweetyTimor.objects.all()
+        context['tweets'] = TweetyTimor.objects.all().annotate(like_count=Count('like'))
         context['count_tweets'] = TweetyTimor.objects.count()
         context['page_template'] = self.page_template_name
         return context
@@ -26,8 +28,8 @@ class TweetyCreate(CreateView):
 class CreateMember(CreateView):
     model = Member
     fields = ['user', 'first_name', 'last_name', 'username', 'password']
-    template_name = 'tweety/login_tweety.html'
-    success_url = reverse_lazy('tweety')
+    template_name = 'tweety/create_members.html'
+    success_url = reverse_lazy('create')
 
     def form_valid(self, form):
         ''' Overriding the form valid method to set the user password correctly '''
@@ -41,11 +43,29 @@ class LikeTweet(CreateView):
     model = Like
     fields = ['tweet']
     template_name = 'tweety/like_tweet.html'
-    success_url = reverse_lazy('tweety')
+    success_url = reverse_lazy('like')
+
+    def form_valid(self, form):
+        if self.request.is_ajax():
+            form.save()
+            # find out how many likes htere are and return to the user
+            count = LikeTweet.objects.filter(tweet=form.instance.tweet_id).count()
+            return HttpResponse(str(count), status=201)
+        else:
+            return super(LikeTweet, self).form_valid(form)
 
 
 class TweetComment(CreateView):
     model = TweetComment
     fields = ['tweet', 'tweetcomment']
     template_name = 'tweety/comment_tweet.html'
-    success_url = reverse_lazy('tweety')
+    success_url = reverse_lazy('comment')
+
+    def form_valid(self, form):
+        if self.request.is_ajax():
+            form.save()
+            # find out how many comments htere are and return to the user
+            count = TweetComment.objects.filter(tweet=form.instance.tweetcomment_id).count()
+            return HttpResponse(str(count), status=201)
+        else:
+            return super(TweetComment, self).form_valid(form)
